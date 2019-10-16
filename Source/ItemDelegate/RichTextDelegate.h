84fd1beb816117a72fd8b8479a644d110af87eb8
Vladimir Kuznetsov <smithcoder@yandex.ru>
2019-10-16 22:02:25 +0300

    0.1.1c2 copyright year

--- Source/ItemDelegate/RichTextDelegate.h
+++ Source/ItemDelegate/RichTextDelegate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2018 Vladimir Kuznetsov <smithcoder@yandex.ru> https://smithcoder.ru/
+ * Copyright (C) 2016-2019 Vladimir Kuznetsov <smithcoder@yandex.ru> https://smithcoder.ru/
  *
  * This file is part of the Smitto.
  *
