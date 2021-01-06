8d9a9447b8553a741d6ab6f1bde673c2d9213510
Vladimir Kuznetsov <smithcoder@yandex.ru>
2021-01-06 22:39:59 +0300

    0.1.1f17 smitto-colors.css

--- Usage/SmittoStyleMaker/SmittoCSSMaker.cpp
+++ Usage/SmittoStyleMaker/SmittoCSSMaker.cpp
@@ -7,14 +7,35 @@
 
 void SmittoCSSMaker()
 {
-	QFile file(QCoreApplication::applicationDirPath().append("/gen/smitto.css"));
+	QFile file(QCoreApplication::applicationDirPath().append("/gen/smitto-colors.css"));
 	if (file.open(QIODevice::WriteOnly))
 	{
+		file.write(":root {\n");
 		for (int i = int(Smitto::SmittoBaseColors::level01); i <= int(Smitto::SmittoBaseColors::level12); ++i)
-			file.write(QString(".sm_t%1 {color:%2;}\n.sm_bg%1 {background:%2;}\n")
+			file.write(QString("--smitto-level-%1: %2;\n")
 					   .arg(i + 1 - int(Smitto::SmittoBaseColors::level01))
 					   .arg(Smitto::scolor(Smitto::SmittoBaseColors(i))).toLocal8Bit());
+		file.write(QString("--smitto-red: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Red)).toLocal8Bit());
+		file.write(QString("--smitto-bold-red: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldRed)).toLocal8Bit());
+		file.write(QString("--smitto-green: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Green)).toLocal8Bit());
+		file.write(QString("--smitto-bold-green: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldGreen)).toLocal8Bit());
+		file.write(QString("--smitto-yellow: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Yellow)).toLocal8Bit());
+		file.write(QString("--smitto-bold-yellow: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldYellow)).toLocal8Bit());
+		file.write(QString("--smitto-blue: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Blue)).toLocal8Bit());
+		file.write(QString("--smitto-bold-blue: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldBlue)).toLocal8Bit());
+		file.write(QString("--smitto-purple: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Purple)).toLocal8Bit());
+		file.write(QString("--smitto-bold-purple: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldPurple)).toLocal8Bit());
+		file.write(QString("--smitto-cyan: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::Cyan)).toLocal8Bit());
+		file.write(QString("--smitto-bold-cyan: %1;\n").arg(Smitto::scolor(Smitto::SmittoBaseColors::BoldCyan)).toLocal8Bit());
+
+		file.write("}\n\n");
+		for (int i = int(Smitto::SmittoBaseColors::level01); i <= int(Smitto::SmittoBaseColors::level12); ++i)
+			file.write(QString(".smitto-text-level-%1 {color: %2;}\n.smitto-background-level-%1 {background: %2;}\n")
+					   .arg(i + 1 - int(Smitto::SmittoBaseColors::level01))
+					   .arg(Smitto::scolor(Smitto::SmittoBaseColors(i))).toLocal8Bit());
+
+		file.write("\n");
 		file.close();
-		qDebug()<<"/gen/smitto.css";
+		qDebug()<<"/gen/smitto-colors.css";
 	}
 }
