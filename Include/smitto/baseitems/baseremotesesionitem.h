a985388c653004eb3d30178a7586c614c0d8dfd3
Vladimir Kuznetsov <smithcoder@yandex.ru>
2021-03-13 22:46:10 +0300

    0.1.1f21 smitto_all_headers && session mistake

--- Include/smitto/baseitems/baseremotesesionitem.h
+++
@@ -1,1 +0,0 @@
-#include "../../../Source/BaseItems/BaseRemoteSesionItem.h"
---
+++ Include/smitto_all_headers.h
@@ -0,0 +1,7 @@
+
+#include <SmittoBase>
+#include <SmittoBaseItems>
+#include <SmittoData>
+#include <SmittoGui>
+#include <SmittoSystem>
+#include <SmittoWidgets>
